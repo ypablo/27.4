b5e2b2733c8e3f5cb042618902fe3d2ff049505b
ypablo <yfedora@gmail.com>
2019-06-19 17:50:36 +0100

    27.4 redux reducer

--- comments.js
+++ comments.js
@@ -4,28 +4,24 @@ import { REMOVE_COMMENT } from './actions';
 import { THUMB_UP_COMMENT } from './actions';
 import { THUMB_DOWN_COMMENT } from './actions';
 
-const initialState = {
-    comments: [],
-    users: []
-};
+const initialState = [];
 
 function reducer(state = initialState, action) {
     switch (action.type) {
         case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                    {
-                        id: action.id,
-                        text: action.text,
-                        votes: 0
-                    }
-                    , ...state]
-            })
+            return [
+                {
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }
+                , ...state]
         case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
+            return comments: state.comments.filter(comment => comment.id !== action.id)
         case EDIT_COMMENT:
+            //znajdz komentarz ktorego id == action.id
+            // map metoda + if id == action.id
+            //text = action.text lub votes +=1
             return Object.assign({}, state, {
                 comments: [
                     {
